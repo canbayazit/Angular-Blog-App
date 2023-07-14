f827287a0bb84b0d3caac99832774f930e479f3c
Can Bayazit <canbayazittitt@gmail.com>
2023-07-14 20:29:25 +0300

    added admin and blog layout components

---
+++ src/app/layout/blog-layout/blog-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { PostService } from 'src/app/services/post.service';
+
+@Component({
+  selector: 'app-blog-layout',
+  templateUrl: './blog-layout.component.html',
+  styleUrls: ['./blog-layout.component.scss']
+})
+export class BlogLayoutComponent {
+
+}
