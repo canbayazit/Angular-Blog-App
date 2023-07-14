ec2b5cc4de7e2211949cc4390d7fa0f89c23a802
Can Bayazit <canbayazittitt@gmail.com>
2023-07-14 20:36:07 +0300

    updated routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlogHomeComponent } from './blog/components/blog-home/blog-home.component';
+import { BlogLayoutComponent } from './layout/blog-layout/blog-layout.component';
+import { AdminLayoutComponent } from './layout/admin-layout/admin-layout.component';
+import { AdminHomeComponent } from './admin/components/admin-home/admin-home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: BlogLayoutComponent,
+    children: [
+      {
+        path: '',
+        component: BlogHomeComponent,
+      },
+    ],
+  },
+  {
+    path: 'admin',
+    component: AdminLayoutComponent,
+    children: [
+      {
+        path: '',
+        component: AdminHomeComponent,
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
