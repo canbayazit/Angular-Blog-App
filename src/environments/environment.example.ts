d753c469d792ac6d65094b0c7ecce5071db773b8
Can Bayazit <canbayazittitt@gmail.com>
2023-07-14 20:24:13 +0300

    added firebase configuration

---
+++ src/environments/environment.example.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'XXXXXXXXXXXX',
+    appId: 'XXXXXXXXXXXX',
+    storageBucket: 'XXXXXXXXXXXX',
+    apiKey: 'XXXXXXXXXXXX',
+    authDomain: 'XXXXXXXXXXXX',
+    messagingSenderId: 'XXXXXXXXXXXX',
+  },
+};
