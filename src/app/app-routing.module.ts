c24841cc61b8b71a44c0f35da391c75cb1ea3e6f
Can Bayazit <canbayazittitt@gmail.com>
2023-07-14 20:39:08 +0300

    updated routing module for blog home component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { BlogHomeComponent } from './blog/components/blog-home/blog-home.component';
+import { BlogHomeComponent } from './blog/pages/blog-home/blog-home.component';
 import { BlogLayoutComponent } from './layout/blog-layout/blog-layout.component';
 import { AdminLayoutComponent } from './layout/admin-layout/admin-layout.component';
 import { AdminHomeComponent } from './admin/components/admin-home/admin-home.component';
