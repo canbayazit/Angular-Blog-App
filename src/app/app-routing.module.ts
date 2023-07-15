7ed38e0321057957dbaa5c4dacf45e54479cbf78
Can Bayazit <rovahelsing@gmail.com>
2023-07-15 23:50:27 +0300

    updated app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,15 +4,40 @@ import { BlogHomeComponent } from './blog/pages/blog-home/blog-home.component';
 import { BlogLayoutComponent } from './layout/blog-layout/blog-layout.component';
 import { AdminLayoutComponent } from './layout/admin-layout/admin-layout.component';
 import { AdminHomeComponent } from './admin/components/admin-home/admin-home.component';
+import { BlogFooterComponent } from './blog/components/blog-footer/blog-footer.component';
+import { BlogPostsComponent } from './blog/pages/blog-posts/blog-posts.component';
+import { BlogPostDetailComponent } from './blog/pages/blog-post-detail/blog-post-detail.component';
+import { BlogCategoriesComponent } from './blog/pages/blog-categories/blog-categories.component';
+import { BlogUsersComponent } from './blog/pages/blog-users/blog-users.component';
 
 const routes: Routes = [
   {
-    path: '',
+    path: "",
     component: BlogLayoutComponent,
     children: [
       {
         path: '',
         component: BlogHomeComponent,
+      },
+      {
+        path: 'posts',
+        component: BlogPostsComponent,
+      },
+      {
+        path: 'posts/detail/:id',
+        component: BlogPostDetailComponent,
+      },
+      {
+        path: 'categories',
+        component: BlogCategoriesComponent,
+      },
+      {
+        path: 'posts/:category',
+        component: BlogPostsComponent,
+      },
+      {
+        path: 'users',
+        component: BlogUsersComponent,
       },
     ],
   },
