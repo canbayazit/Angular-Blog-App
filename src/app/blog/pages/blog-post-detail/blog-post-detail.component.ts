50e144502258cf6b0bbedda05b2d9d7fa8599893
Can Bayazit <rovahelsing@gmail.com>
2023-07-16 00:18:01 +0300

    added blog post detail page component

---
+++ src/app/blog/pages/blog-post-detail/blog-post-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-post-detail',
+  templateUrl: './blog-post-detail.component.html',
+  styleUrls: ['./blog-post-detail.component.scss']
+})
+export class BlogPostDetailComponent {
+
+}
