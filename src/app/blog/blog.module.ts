ef061af98fcd7b36622c73bba4a0e4b9fc437f97
Can Bayazit <rovahelsing@gmail.com>
2023-07-16 21:33:12 +0300

    updated blog module

--- src/app/blog/blog.module.ts
+++ src/app/blog/blog.module.ts
@@ -1,12 +1,18 @@
-import { NgModule, forwardRef } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BlogHeaderComponent } from './components/blog-header/blog-header.component';
 import { BlogSidebarComponent } from './components/blog-sidebar/blog-sidebar.component';
 import { BlogFooterComponent } from './components/blog-footer/blog-footer.component';
 import { BlogLayoutComponent } from '../layout/blog-layout/blog-layout.component';
-import { BlogHomeComponent } from './pages/blog-home/blog-home.component';
 import { AppRoutingModule } from '../app-routing.module';
-import { PostService } from '../services/post.service';
+import { MaterialUiModule } from '../material-ui/material-ui.module';
+import { BlogPostsComponent } from './pages/blog-posts/blog-posts.component';
+import { BlogUsersComponent } from './pages/blog-users/blog-users.component';
+import { BlogCategoriesComponent } from './pages/blog-categories/blog-categories.component';
+import { BlogPostDetailComponent } from './pages/blog-post-detail/blog-post-detail.component';
+import { BlogHomeComponent } from './pages/blog-home/blog-home.component';
+import { RouterModule } from '@angular/router';
+import { BlogCardComponent } from './components/blog-card/blog-card.component';
 
 
 @NgModule({
@@ -16,11 +22,18 @@ import { PostService } from '../services/post.service';
     BlogHeaderComponent,
     BlogSidebarComponent,
     BlogFooterComponent,
+    BlogPostsComponent,
+    BlogUsersComponent,
+    BlogCategoriesComponent,
+    BlogPostDetailComponent,
+    BlogCardComponent,
 
   ],
   imports: [
     CommonModule,
     AppRoutingModule,
+    MaterialUiModule,
+    RouterModule
   ],
   providers:[]
 })
