f767b6e5f851332b4d0aca6b95f8c527acd554d2
Can Bayazit <rovahelsing@gmail.com>
2023-07-16 21:31:55 +0300

    updated blog post page with design and service codes

--- src/app/blog/pages/blog-posts/blog-posts.component.ts
+++ src/app/blog/pages/blog-posts/blog-posts.component.ts
@@ -1,20 +1,55 @@
 import { Component, OnInit } from '@angular/core';
+import ICategory from 'src/app/model/category/category';
+import IPost from 'src/app/model/post/post';
+import { CategoryService } from 'src/app/services/category-service/category.service';
 import { PostService } from 'src/app/services/post-service/post.service';
 
 @Component({
   selector: 'app-blog-posts',
   templateUrl: './blog-posts.component.html',
-  styleUrls: ['./blog-posts.component.scss']
+  styleUrls: ['./blog-posts.component.scss'],
 })
-export class BlogPostsComponent implements OnInit{
+export class BlogPostsComponent implements OnInit {
+  categoryList: ICategory[] = [];
+  postList: IPost[] = [];
   constructor(
-    private postsService: PostService,
-  ){
+    private categoriesService: CategoryService,
+    private postsService: PostService
+  ) {}
+  ngOnInit() {
+    this.categoriesService.getCategories().subscribe((categories) => {
+      this.categoryList = categories.map((category) => {
+        const { category_id, creation_date, name } = category;
+        return { category_id, creation_date, name } as ICategory;
+      });
+      console.log(this.categoryList);
+    });
 
+    this.postsService.getPosts().subscribe((posts) => {
+      this.postList = posts.map((post) => {
+        const {
+          category_id,
+          content,
+          creation_date,
+          is_published,
+          post_id,
+          title,
+          user_id,
+          view_count,
+        } = post;
+        return {
+          category_id,
+          content,
+          creation_date,
+          is_published,
+          post_id,
+          title,
+          user_id,
+          view_count,
+        } as IPost;
+      });
+      console.log(posts);
+    });
   }
-  ngOnInit(){
-    this.postsService.getPosts().subscribe(posts=>{
-      console.log(posts)
-    })
-  }
+  applyFilter(filter: string) {}
 }
