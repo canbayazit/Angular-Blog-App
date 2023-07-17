3159ce3a5a916468552f4873573ec8d5327c4766
Can Bayazit <rovahelsing@gmail.com>
2023-07-17 21:32:40 +0300

    added category service

--- src/app/services/category-service/category.service.ts
+++ src/app/services/category-service/category.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -9,5 +10,13 @@ export class CategoryService {
   getCategories() {
     const collectionInstance = collection(this.firestore, 'categories');
     return collectionData(collectionInstance); // bu fonksiyon observable dönüyor
+  }
+  getCategoryById(id: number) {
+    const collectionInstance = collection(this.firestore, 'categories');
+    return collectionData(collectionInstance).pipe(
+      map((documents) =>
+        documents.filter((doc) => (doc['category_id']===id))
+      )
+    );
   }
 }
