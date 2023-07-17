1ad3ebb3d83c262f35c6ea5c1eb9b6a96225f098
Can Bayazit <rovahelsing@gmail.com>
2023-07-17 21:34:10 +0300

    updated post service

--- src/app/services/post-service/post.service.ts
+++ src/app/services/post-service/post.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 // firebase/firestore kısmından // import etmeye çalıştığım için 2 saat nerde hata
 // yapıyorum diye uğraşmak zorunda kaldım.
 import { Firestore, collection, collectionData, limit, orderBy, query, startAfter } from '@angular/fire/firestore';
+import { map } from 'rxjs';
 
 // Firebase üzerinden canlı olarak verilerimizi çektiğimiz post service.
 @Injectable({ providedIn: 'root' })
@@ -11,5 +12,17 @@ export class PostService {
   getPosts() {
     const collectionInstance = collection(this.firestore, 'posts');
     return collectionData(collectionInstance); // bu fonksiyon observable dönüyor
+  }
+  getPostById(id: number) {
+    console.log(id,"geliyor mu")
+    const collectionInstance = collection(this.firestore, 'posts');
+    return collectionData(collectionInstance).pipe(
+      map((documents) =>
+        documents.filter((doc) => {
+          console.log((doc['post_id'],id,"IDLER"))
+          return doc['post_id']===id
+        })
+      )
+    );
   }
 }
