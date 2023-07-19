ebe36a54bdeb87663c6486dcdadb7295bcb678c4
Can Bayazit <rovahelsing@gmail.com>
2023-07-20 00:06:27 +0300

    updated routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { BlogHomeComponent } from './blog/pages/blog-home/blog-home.component';
 import { BlogLayoutComponent } from './layout/blog-layout/blog-layout.component';
 import { AdminLayoutComponent } from './layout/admin-layout/admin-layout.component';
-import { AdminHomeComponent } from './admin/components/admin-home/admin-home.component';
 import { BlogFooterComponent } from './blog/components/blog-footer/blog-footer.component';
 import { BlogPostsComponent } from './blog/pages/blog-posts/blog-posts.component';
 import { BlogPostDetailComponent } from './blog/pages/blog-post-detail/blog-post-detail.component';
 import { BlogCategoriesComponent } from './blog/pages/blog-categories/blog-categories.component';
 import { BlogUsersComponent } from './blog/pages/blog-users/blog-users.component';
+import { AdminCategoryComponent } from './admin/pages/admin-category/admin-category.component';
+import { AdminCommentsComponent } from './admin/pages/admin-comments/admin-comments.component';
+import { AdminPostsComponent } from './admin/pages/admin-posts/admin-posts.component';
+import { AdminUserComponent } from './admin/pages/admin-user/admin-user.component';
 
 const routes: Routes = [
   {
@@ -46,8 +49,20 @@ const routes: Routes = [
     component: AdminLayoutComponent,
     children: [
       {
-        path: '',
-        component: AdminHomeComponent,
+        path: 'user',
+        component: AdminUserComponent,
+      },
+      {
+        path: 'post',
+        component: AdminPostsComponent,
+      },
+      {
+        path: 'category',
+        component: AdminCategoryComponent,
+      },
+      {
+        path: 'comment',
+        component: AdminCommentsComponent,
       },
     ],
   },
