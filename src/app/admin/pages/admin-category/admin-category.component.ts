8cb348000e7c2994e1d0cc0b96e80fb3e5ebd177
Can Bayazit <rovahelsing@gmail.com>
2023-07-20 00:13:24 +0300

    added category component

---
+++ src/app/admin/pages/admin-category/admin-category.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-category',
+  templateUrl: './admin-category.component.html',
+  styleUrls: ['./admin-category.component.scss']
+})
+export class AdminCategoryComponent {
+  isDetailsVisible:boolean = true;
+
+  toggleDetails() {
+    this.isDetailsVisible = !this.isDetailsVisible;
+  }
+}
