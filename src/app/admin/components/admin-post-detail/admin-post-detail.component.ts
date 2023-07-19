f7fc4d55a7db615f5c4db8cf7ad32a3b87b53d06
Can Bayazit <rovahelsing@gmail.com>
2023-07-20 00:11:36 +0300

    added post detail component

---
+++ src/app/admin/components/admin-post-detail/admin-post-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-post-detail',
+  templateUrl: './admin-post-detail.component.html',
+  styleUrls: ['./admin-post-detail.component.scss']
+})
+export class AdminPostDetailComponent {
+
+}
