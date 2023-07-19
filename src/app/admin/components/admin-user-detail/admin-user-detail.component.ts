fdfeb62820bdcd553c60801805cc0f7bbf08e76e
Can Bayazit <rovahelsing@gmail.com>
2023-07-20 00:12:45 +0300

    added admin user detail component

---
+++ src/app/admin/components/admin-user-detail/admin-user-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-user-detail',
+  templateUrl: './admin-user-detail.component.html',
+  styleUrls: ['./admin-user-detail.component.scss']
+})
+export class AdminUserDetailComponent {
+
+}
