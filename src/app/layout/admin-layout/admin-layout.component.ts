55c4b19201d996244149d46093068aa76f1e533d
Can Bayazit <rovahelsing@gmail.com>
2023-07-20 00:19:06 +0300

    updated admin layout component

--- src/app/layout/admin-layout/admin-layout.component.ts
+++ src/app/layout/admin-layout/admin-layout.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ThemePalette } from '@angular/material/core';
 
 @Component({
   selector: 'app-admin-layout',
   templateUrl: './admin-layout.component.html',
   styleUrls: ['./admin-layout.component.scss']
 })
-export class AdminLayoutComponent {
+export class AdminLayoutComponent implements OnInit {
+  links = [
+    { name: 'USERS', path: 'user' },
+    { name: 'POSTS', path: 'post' },
+    { name: 'COMMENTS', path: 'comment' },
+    { name: 'CATEGORIES', path: 'category' },
+  ];
+  activeLink:any;
+  background: ThemePalette = undefined;
+  ngOnInit() {
+    this.activeLink = this.links?.find(item=>{
+      return "/admin/"+item.path === location.pathname
+    })
+  }
 
 }
