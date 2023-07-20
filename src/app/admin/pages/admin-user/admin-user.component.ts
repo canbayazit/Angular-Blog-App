798fc90d52c2a3a8e21642b92d3be34f4c2abd46
Can Bayazit <rovahelsing@gmail.com>
2023-07-20 07:06:08 +0300

    added pagination structure to admin user component

--- src/app/admin/pages/admin-user/admin-user.component.ts
+++ src/app/admin/pages/admin-user/admin-user.component.ts
@@ -1,16 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { from, map, mergeMap, skip, take, toArray } from 'rxjs';
+import IUser from 'src/app/model/user/user';
+import { UserService } from 'src/app/services/user-service/user.service';
 
 @Component({
   selector: 'app-admin-user',
   templateUrl: './admin-user.component.html',
-  styleUrls: ['./admin-user.component.scss']
+  styleUrls: ['./admin-user.component.scss'],
 })
-export class AdminUserComponent {
-  isDetailsVisible:boolean = true;
+export class AdminUserComponent implements OnInit {
+  isDetailsVisible: boolean = true;
+  userList: IUser[] = [];
+  currentPage: number = 1;
+  pageSize: number = 10;
+  selectedIndex: number = -1;
 
+
+  constructor(private userService: UserService) {}
+  ngOnInit() {
+    this.loadUsers();
+  }
   toggleDetails() {
     this.isDetailsVisible = !this.isDetailsVisible;
   }
+  isSelected(index: number) {
+    if (this.selectedIndex === index) {
+      this.selectedIndex = -1; 
+    } else {
+      this.selectedIndex = index;
+    }
+  }
+  loadUsers() {
+    this.userService.getUserByPaginator(this.currentPage, this.pageSize).subscribe((result) => {
+      this.userList = result.map((user) => {
+        const { creation_date, email, is_active, user_id, username } = user;
+        return {
+          creation_date,
+          email,
+          is_active,
+          user_id,
+          username,
+        } as IUser;
+      });
+    });
+  }
 
-  
+
+  previousPage() {
+    if (this.currentPage > 1) {
+      this.currentPage--;
+      this.loadUsers();
+    }
+  }
+
+  nextPage() {
+    console.log(this.pageSize,"this.pageSize")
+    console.log(this.currentPage,"this.currentPage")
+    console.log(this.userList.length,"this.length")
+    if (this.userList.length >= this.pageSize) {
+      this.currentPage++;
+      this.loadUsers();
+    }
+  }
+
 }
