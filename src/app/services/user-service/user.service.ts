dc99514b9db5997cdb32a618288382dde9727b47
Can Bayazit <rovahelsing@gmail.com>
2023-07-21 01:09:57 +0300

    added update method for user service

--- src/app/services/user-service/user.service.ts
+++ src/app/services/user-service/user.service.ts
@@ -4,7 +4,10 @@ import {
   collection,
   collectionData,
   doc,
-  getDoc,
+  getDocs,
+  query,
+  updateDoc,
+  where,
 } from '@angular/fire/firestore';
 import { from, map, mergeMap, skip, take, toArray } from 'rxjs';
 import IUser from 'src/app/model/user/user';
@@ -33,5 +36,32 @@ export class UserService {
         documents.filter((doc) => (doc['user_id']===id))
       )
     );
+  }
+
+  async updateUser(updatedComment: any) {
+    try {
+      // Query for the document with the matching comment_id
+      const q = query(collection(this.firestore, 'users'), where('user_id', '==', updatedComment.user_id));
+      const querySnapshot = await getDocs(q);
+
+      if (!querySnapshot.empty) {
+        // Get the first document (assuming there is only one with the same comment_id)
+        const docSnapshot = querySnapshot.docs[0];
+        const commentDocRef = doc(this.firestore, 'users', docSnapshot.id);
+
+        // Update the document with the new data
+        await updateDoc(commentDocRef, updatedComment);
+
+        return true;
+      } else {
+        // Handle the case where no document with the matching comment_id is found
+        console.error('Comment not found with user_id:', updatedComment.user_id);
+        return false;
+      }
+    } catch (error) {
+      // Handle any errors that may occur during the update process
+      console.error('Error updating user:', error);
+      return false;
+    }
   }
 }
