f69df17e45d62bd106f72af13e8e1bc32089088f
Can Bayazit <rovahelsing@gmail.com>
2023-07-20 20:03:24 +0300

    updated post service

--- src/app/services/post-service/post.service.ts
+++ src/app/services/post-service/post.service.ts
@@ -13,16 +13,36 @@ export class PostService {
     const collectionInstance = collection(this.firestore, 'posts');
     return collectionData(collectionInstance); // bu fonksiyon observable dönüyor
   }
-  getPostById(id: number) {
-    console.log(id,"geliyor mu")
+  getPostById(id: number,type:string) {
     const collectionInstance = collection(this.firestore, 'posts');
-    return collectionData(collectionInstance).pipe(
-      map((documents) =>
-        documents.filter((doc) => {
-          console.log((doc['post_id'],id,"IDLER"))
-          return doc['post_id']===id
-        })
-      )
-    );
+    if (type==="post") {
+      return collectionData(collectionInstance).pipe(
+        map((documents) =>
+          documents.filter((doc) => {
+            console.log((doc['post_id'],id,"IDLER"))
+            return doc['post_id']===id
+          })
+        )
+      );
+    }else if(type==="user"){
+      return collectionData(collectionInstance).pipe(
+        map((documents) =>
+          documents.filter((doc) => {
+            console.log((doc['user_id'],id,"IDLER"))
+            return doc['user_id']===id
+          })
+        )
+      );
+    }else if(type==="category"){
+      return collectionData(collectionInstance).pipe(
+        map((documents) =>
+          documents.filter((doc) => {
+            console.log((doc['category_id'],id,"IDLER"))
+            return doc['category_id']===id
+          })
+        )
+      );
+    }
+    return
   }
 }
