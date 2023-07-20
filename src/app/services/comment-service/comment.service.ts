21be350fd44e12c7fc4c1e32405e0eb24b5d9adb
Can Bayazit <rovahelsing@gmail.com>
2023-07-20 20:04:13 +0300

    updated comment service, added update comment method

--- src/app/services/comment-service/comment.service.ts
+++ src/app/services/comment-service/comment.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Firestore, collection, collectionData, doc, getDocs, query, updateDoc, where } from '@angular/fire/firestore';
 import { map } from 'rxjs';
 
 @Injectable({
@@ -12,12 +12,79 @@ export class CommentService {
     const collectionInstance = collection(this.firestore, 'comments');
     return collectionData(collectionInstance); // bu fonksiyon observable dönüyor
   }
-  getAllCommentByPostId(id:number){
+  getCommentById(id:number,type:string){
     const collectionInstance = collection(this.firestore, 'comments');
-    return collectionData(collectionInstance).pipe(
-      map((documents) =>
-        documents.filter((doc) => (doc['post_id']===id))
-      )
-    );
+    if (type==="post") {
+      return collectionData(collectionInstance).pipe(
+        map((documents) =>
+          documents.filter((doc) => (doc['post_id']===id))
+        )
+      );
+    }else if(type==="user"){
+      return collectionData(collectionInstance).pipe(
+        map((documents) =>
+          documents.filter((doc) => (doc['user_id']===id))
+        )
+      );
+    }
+    return
   }
+
+  async updateComment(updatedComment: any) {
+    try {
+      // Query for the document with the matching comment_id
+      const q = query(collection(this.firestore, 'comments'), where('comment_id', '==', updatedComment.comment_id));
+      const querySnapshot = await getDocs(q);
+
+      if (!querySnapshot.empty) {
+        // Get the first document (assuming there is only one with the same comment_id)
+        const docSnapshot = querySnapshot.docs[0];
+        const commentDocRef = doc(this.firestore, 'comments', docSnapshot.id);
+
+        // Update the document with the new data
+        await updateDoc(commentDocRef, updatedComment);
+
+        return true;
+      } else {
+        // Handle the case where no document with the matching comment_id is found
+        console.error('Comment not found with comment_id:', updatedComment.comment_id);
+        return false;
+      }
+    } catch (error) {
+      // Handle any errors that may occur during the update process
+      console.error('Error updating comment:', error);
+      return false;
+    }
+  }
+
+  // try {
+  //   // Query for the document with the matching commentId
+  //   const q = query(
+  //     collection(this.firestore, 'comments'),
+  //     where('comment_id', '==', updatedComment.comment_id.toString())
+  //   );
+  //   console.log(updatedComment.comment_id,"updatedComment.comment_id")
+  //   console.log(q,"q")
+
+  //   const querySnapshot = await getDocs(q);
+
+  //   // Get the first document (assuming there is only one with the same commentId)
+  //   const docSnapshot = querySnapshot.docs[0];
+  //     console.log(docSnapshot,"docSnapshot")
+  //   if (docSnapshot) {
+  //     const commentDocRef = doc(this.firestore, 'comments', docSnapshot.id);
+  //     return updateDoc(commentDocRef, updatedComment);
+  //   } else {
+  //     // Handle the case where no document with the matching commentId is found
+  //     console.error(
+  //       'Comment not found with comment_id:',
+  //       updatedComment.comment_id
+  //     );
+  //     return null;
+  //   }
+  // } catch (error) {
+  //   // Handle any errors that may occur during the update process
+  //   console.error('Error updating comment:', error);
+  //   return null;
+  // }
 }
