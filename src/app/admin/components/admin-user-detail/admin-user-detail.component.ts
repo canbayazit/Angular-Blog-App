c7e881deea0264296e27aec1dc423755a8b63a5a
Can Bayazit <rovahelsing@gmail.com>
2023-07-20 20:27:42 +0300

    added services to load comments and posts of the relevant user

--- src/app/admin/components/admin-user-detail/admin-user-detail.component.ts
+++ src/app/admin/components/admin-user-detail/admin-user-detail.component.ts
@@ -1,10 +1,85 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import IComment from 'src/app/model/comment/comment';
+import IPost from 'src/app/model/post/post';
+import { CommentService } from 'src/app/services/comment-service/comment.service';
+import { PostService } from 'src/app/services/post-service/post.service';
 
 @Component({
   selector: 'app-admin-user-detail',
   templateUrl: './admin-user-detail.component.html',
-  styleUrls: ['./admin-user-detail.component.scss']
+  styleUrls: ['./admin-user-detail.component.scss'],
 })
-export class AdminUserDetailComponent {
+export class AdminUserDetailComponent implements OnChanges {
+  @Input() userId?: number;
+  postList: IPost[] = [];
+  commentList: IComment[] = [];
+  constructor(
+    private postService:PostService,
+    private commentService:CommentService
+  ){
 
+  }
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['userId'] && changes['userId'].currentValue) {
+      this.loadPosts(this.userId);
+      this.loadComments(this.userId);
+    }
+  }
+
+  loadPosts(userId?: number) {
+    if (userId) {
+      console.log(userId,"karşılaştır")
+      this.postService.getPostById(userId, 'user')?.subscribe((posts) => {
+        this.postList = posts.map((post) => {
+          const {
+            category_id,
+            content,
+            creation_date,
+            is_published,
+            post_id,
+            title,
+            user_id,
+            view_count,
+          } = post;
+          return {
+            category_id,
+            content,
+            creation_date,
+            is_published,
+            post_id,
+            title,
+            user_id,
+            view_count,
+          } as IPost;
+        });
+      });
+    }
+
+  }
+  loadComments(userId?:number){
+    if (userId) {
+      this.commentService.getCommentById(userId, 'user')?.subscribe((comments) => {
+        this.commentList = comments.map((commentData) => {
+          const {
+            comment,
+            comment_id,
+            creation_date,
+            is_confirmed,
+            post_id,
+            user_id,
+            username,
+          } = commentData;
+          return {
+            comment,
+            comment_id,
+            creation_date,
+            is_confirmed,
+            post_id,
+            user_id,
+            username,
+          } as IComment;
+        });
+      });
+    }
+  }
 }
