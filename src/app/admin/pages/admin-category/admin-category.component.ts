b22a4d5b8a5eff3faf5e06cee6dec17d2fda8a83
Can Bayazit <rovahelsing@gmail.com>
2023-07-22 02:43:30 +0300

    updated category page component according to services. Also added dialog configuration and design

--- src/app/admin/pages/admin-category/admin-category.component.ts
+++ src/app/admin/pages/admin-category/admin-category.component.ts
@@ -1,14 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { AdminAddCategoryDialogComponent } from '../../components/admin-add-category-dialog/admin-add-category-dialog.component';
+import { CategoryService } from 'src/app/services/category-service/category.service';
+import ICategory from 'src/app/model/category/category';
+import { AdminEditCategoryDialogComponent } from '../../components/admin-edit-category-dialog/admin-edit-category-dialog.component';
 
 @Component({
   selector: 'app-admin-category',
   templateUrl: './admin-category.component.html',
   styleUrls: ['./admin-category.component.scss']
 })
-export class AdminCategoryComponent {
+export class AdminCategoryComponent implements OnInit {
   isDetailsVisible:boolean = true;
+  categoryList: ICategory[] = [];
+  constructor(
+    private dialog: MatDialog,
+    private categoryService: CategoryService,
+    ) {}
+    ngOnInit() {
+      this.loadUsers();
+    }
+    loadUsers() {
+      this.categoryService.getCategories().subscribe((categories) => {
+        this.categoryList = categories.map((category) => {
+          const { category_id, creation_date, name } = category;
+          return { category_id, creation_date, name } as ICategory;
+        });
+      })
+  }
 
   toggleDetails() {
     this.isDetailsVisible = !this.isDetailsVisible;
+  }
+  openEditModal(category:ICategory): void {
+    const dialogRef = this.dialog.open(AdminEditCategoryDialogComponent, {
+      width:'60%',
+      data: category,  // send data to dialog component
+    });
+
+    dialogRef.afterClosed().subscribe((updatedComment) => {
+      // Update the comment data if needed
+      if (updatedComment) {
+        // this.comment = updatedComment;
+      }
+    });
+  }
+  openAddModal(): void {
+    const dialogRef = this.dialog.open(AdminAddCategoryDialogComponent, {
+      width:'60%',
+        // send data to dialog component
+    });
+
+    dialogRef.afterClosed().subscribe((updatedComment) => {
+      // Update the comment data if needed
+      if (updatedComment) {
+        // this.comment = updatedComment;
+      }
+    });
   }
 }
