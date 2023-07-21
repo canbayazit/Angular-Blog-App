21219f27f5130aeafbc6bbf1cb4cb39dd348f4ab
Can Bayazit <rovahelsing@gmail.com>
2023-07-22 02:08:40 +0300

    added new service methods to post service.

--- src/app/services/post-service/post.service.ts
+++ src/app/services/post-service/post.service.ts
@@ -2,13 +2,21 @@ import { Injectable } from '@angular/core';
 // Önemli Importları @angular/fire/firestore' üzerinden çekin çünkü ben yanlışlıkla
 // firebase/firestore kısmından // import etmeye çalıştığım için 2 saat nerde hata
 // yapıyorum diye uğraşmak zorunda kaldım.
-import { Firestore, collection, collectionData, limit, orderBy, query, startAfter } from '@angular/fire/firestore';
+import { Firestore, collection, collectionData, doc, getDocs, limit, orderBy, query, startAfter, updateDoc, where } from '@angular/fire/firestore';
 import { map } from 'rxjs';
 
 // Firebase üzerinden canlı olarak verilerimizi çektiğimiz post service.
 @Injectable({ providedIn: 'root' })
 export class PostService {
   constructor(private firestore: Firestore) {}
+  getPostByPaginator(page:number,pageSize:number){
+    const collectionInstance = collection(this.firestore, 'posts');
+    return collectionData(collectionInstance).pipe(
+      map((posts) => {
+        return  posts.slice((page - 1) * pageSize, page * pageSize);
+      })
+    );
+  }
   getPosts() {
     const collectionInstance = collection(this.firestore, 'posts');
     return collectionData(collectionInstance); // bu fonksiyon observable dönüyor
@@ -41,5 +49,32 @@ export class PostService {
       );
     }
     return
+  }
+
+  async updatePost(updatedComment: any) {
+    try {
+      // Query for the document with the matching comment_id
+      const q = query(collection(this.firestore, 'posts'), where('post_id', '==', updatedComment.post_id));
+      const querySnapshot = await getDocs(q);
+
+      if (!querySnapshot.empty) {
+        // Get the first document (assuming there is only one with the same comment_id)
+        const docSnapshot = querySnapshot.docs[0];
+        const commentDocRef = doc(this.firestore, 'posts', docSnapshot.id);
+
+        // Update the document with the new data
+        await updateDoc(commentDocRef, updatedComment);
+
+        return true;
+      } else {
+        // Handle the case where no document with the matching comment_id is found
+        console.error('Post not found with post_id:', updatedComment.user_id);
+        return false;
+      }
+    } catch (error) {
+      // Handle any errors that may occur during the update process
+      console.error('Error updating post:', error);
+      return false;
+    }
   }
 }
