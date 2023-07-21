16e30e2fcbc3aa4cfa5bb6c54b014bd7c359b8d5
Can Bayazit <rovahelsing@gmail.com>
2023-07-22 02:50:36 +0300

    sended from parent to child component.

--- src/app/admin/components/admin-comment-detail/admin-comment-detail.component.ts
+++ src/app/admin/components/admin-comment-detail/admin-comment-detail.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import IPost from 'src/app/model/post/post';
 
 @Component({
   selector: 'app-admin-comment-detail',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./admin-comment-detail.component.scss']
 })
 export class AdminCommentDetailComponent {
-
+  @Input() post?: IPost;
+  @Input() userId?: number;
 }
--- src/app/admin/components/admin-post-detail/admin-post-detail.component.ts
+++ src/app/admin/components/admin-post-detail/admin-post-detail.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import IPost from 'src/app/model/post/post';
 
 @Component({
   selector: 'app-admin-post-detail',
   templateUrl: './admin-post-detail.component.html',
-  styleUrls: ['./admin-post-detail.component.scss']
+  styleUrls: ['./admin-post-detail.component.scss'],
 })
 export class AdminPostDetailComponent {
-
+  @Input() post?: IPost;
 }
