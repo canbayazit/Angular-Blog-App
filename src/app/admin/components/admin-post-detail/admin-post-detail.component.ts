dcca9a9447176de23565890831abe7806a396f0c
Can Bayazit <rovahelsing@gmail.com>
2023-07-24 03:07:50 +0300

    created the route mechanism for components related to post.

--- src/app/admin/components/admin-post-detail/admin-post-detail.component.ts
+++ src/app/admin/components/admin-post-detail/admin-post-detail.component.ts
@@ -1,11 +1,56 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import IPost from 'src/app/model/post/post';
+import { PostService } from 'src/app/services/post-service/post.service';
 
 @Component({
   selector: 'app-admin-post-detail',
   templateUrl: './admin-post-detail.component.html',
   styleUrls: ['./admin-post-detail.component.scss'],
 })
-export class AdminPostDetailComponent {
-  @Input() post?: IPost;
+export class AdminPostDetailComponent implements OnInit {
+  postList: IPost[] = [];
+  constructor(
+    // private commentService: CommentService,
+    private postService: PostService,
+    private route: ActivatedRoute
+
+  ) {}
+  ngOnInit() {
+    this.route.params.subscribe((params) => {
+      const id = params['id'];
+      this.loadPosts(parseInt(id));
+        // this.loadComments(parseInt(id));
+    })
+  }
+
+  loadPosts(postId?: number) {
+    if (postId) {
+      this.postService.getPostById(postId, 'post')?.subscribe((posts) => {
+        this.postList = posts.map((post) => {
+          const {
+            category_id,
+            content,
+            creation_date,
+            is_published,
+            post_id,
+            title,
+            user_id,
+            view_count,
+          } = post;
+          return {
+            category_id,
+            content,
+            creation_date,
+            is_published,
+            post_id,
+            title,
+            user_id,
+            view_count,
+          } as IPost;
+        });
+      });
+    }
+
+  }
 }
